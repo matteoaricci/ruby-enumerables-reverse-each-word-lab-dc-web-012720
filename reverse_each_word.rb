16a91e802554f55d6fd9102d91e863c72cfc403f
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 21:42:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(sentence)
-  rev_sent = sentence.split(" ")
-  new_arr = []
+  # rev_sent = sentence.split(" ")
+  # new_arr = []
   
-  rev_sent.each do |n|
-    new_arr << n.reverse 
-  end 
-  new_arr.join(" ")
+  # rev_sent.each do |n|
+  #   new_arr << n.reverse 
+  # end 
+  # new_arr.join(" ")
   
   rev_sent.collect do |n|
     n.reverse
